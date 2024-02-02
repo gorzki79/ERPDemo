97f579fdfaad4a58072a1e03b49ba75b5a4616b2
Marcin Gorzkiewicz <gorzki79@live.com>
2024-02-02 19:50:37 +0100

    Added IQuery and BaseQueryHandler in Persistence.

---
+++ ERPDemo.Application/Queries/IQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace ERPDemo.Application.Queries
+{
+    public interface IQuery<out TQueryResult> : IRequest<TQueryResult>
+    {
+    }
+}
---
+++ ERPDemo.Persistence/Query/Handlers/BaseQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using ERPDemo.Application.Queries;
+using ERPDemo.Persistence.Data.Entities;
+using MediatR;
+
+namespace ERPDemo.Persistence.Query.Handlers
+{
+    public abstract class BaseQueryHandler<TQuery, TResult> : IRequestHandler<TQuery, TResult>
+        where TQuery : IQuery<TResult>
+    {
+        protected readonly ErpDemoDbContext DbContext;
+
+        protected BaseQueryHandler(ErpDemoDbContext dbContext)
+        {
+            this.DbContext = dbContext;
+        }
+
+        public abstract Task<TResult> Handle(TQuery request, CancellationToken cancellationToken);
+    }
+}
