162a9623ef2ad3f4fa07b0e1d5d845a50e120332
Marcin Gorzkiewicz <gorzki79@live.com>
2024-02-02 19:55:38 +0100

    Added DB setup to app.

---
+++ ERPDemo.Persistence/AppExtensions.cs
@@ -0,0 +1,24 @@
+﻿using ERPDemo.Persistence.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ERPDemo.Persistence
+{
+    public static class AppExtensions
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services)
+        {
+            services.AddDbContextPool<ErpDemoDbContext>(options =>
+            {
+                var serviceProvider = services.BuildServiceProvider();
+                IConfiguration configuration = serviceProvider.GetRequiredService<IConfiguration>();
+
+                options.EnableSensitiveDataLogging(configuration.GetValue<bool>("SqlLogSensitiveData"));
+                options.UseSqlServer(configuration.GetConnectionString("ERPDb"));
+            });
+
+            return services;
+        }
+    }
+}
