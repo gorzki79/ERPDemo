b8cbec64ba5908a44343bfb81570f0bbcf5fa2d7
Marcin Gorzkiewicz <gorzki79@live.com>
2024-02-03 08:45:35 +0100

    Defined ITruckRepository.

---
+++ ERPDemo.Core/Repositories/ITruckRepository.cs
@@ -0,0 +1,13 @@
+﻿using ERPDemo.Core.Entities;
+using ERPDemo.Core.ValueObjects;
+
+namespace ERPDemo.Core.Repositories
+{
+    public interface ITruckRepository
+    {
+        Task<Truck> GetTruckAsync(TruckId truckId, CancellationToken cancellationToken);
+        Task CreateTruckAsync(Truck truck, CancellationToken cancellationToken);
+        Task UpdateTruckAsync(Truck truck, CancellationToken cancellationToken);
+        Task DeleteTruckAsync(TruckId truckId, CancellationToken cancellationToken);
+    }
+}
